96b133f45e0a45f2ff6b719edd5e192fc288b653
yezijuns <1559411344@qq.com>
2022-05-22 11:52:18 +0800

    day-8

---
+++ 链表和数组/535.tiny-url-的加密与解密.js
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=535 lang=javascript
+ *
+ * [535] TinyURL 的加密与解密
+ */
+
+// @lc code=start
+/**
+ * Encodes a URL to a shortened URL.
+ *
+ * @param {string} longUrl
+ * @return {string}
+ */
+var encode = function(longUrl) {
+    
+};
+
+/**
+ * Decodes a shortened URL to its original URL.
+ *
+ * @param {string} shortUrl
+ * @return {string}
+ */
+var decode = function(shortUrl) {
+    
+};
+
+/**
+ * Your functions will be called as such:
+ * decode(encode(url));
+ */
+// @lc code=end
+
